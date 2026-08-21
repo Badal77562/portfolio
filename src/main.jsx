24737a1ea18aa7256a1eb8ca0c6bf4b7dd3f76e0
Badal77562 <badalmahata46@gmail.com>
2026-08-21 16:37:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,5 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
         <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
                   <App />
-        </BrowserRouter>>
+        </BrowserRouter>
       );
